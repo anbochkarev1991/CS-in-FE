69a555fae5a58b425a368f88b885f4635410f979
Anton Bochkarev <a.n.bochkarev1991@gmail.com>
2023-05-03 21:57:56 -0300

    stack done

---
+++ src/fourth-lecture/stack/stack.ts
@@ -0,0 +1,72 @@
+class Stack {
+  array: any;
+  elementsType: string | undefined;
+  headIndex = -1;
+  head: unknown = null;
+  maxLength: number;
+
+  constructor(typedArrayConstructor: any, size: number) {
+    this.array = new typedArrayConstructor(size);
+    this.maxLength = size;
+  }
+
+  push(value: number | string) {
+    if (this.headIndex === this.maxLength - 1) {
+      throw new Error('Maximum length exceeded');
+    }
+
+    if (!this.elementsType) {
+      this.elementsType = typeof value;
+    } else if (this.elementsType !== typeof value) {
+      throw new Error(`This value ${value} type differs from other elements in the stack`);
+    }
+
+    if (typeof value === 'string' && value.length > 1) {
+      throw new Error('Cannot add more than 1 string character');
+    }
+
+    this.headIndex++;
+    this.head = value;
+
+    this.array[this.headIndex]
+      |= typeof value === 'string' ? value.charCodeAt(0) : value;
+  }
+
+  pop() {
+    if (this.headIndex === -1) {
+      throw new Error('No more elements in stack');
+    }
+
+    const deleted = (this.elementsType === 'string')
+      ? String.fromCharCode(this.array[this.headIndex])
+      : this.array[this.headIndex];
+
+    this.headIndex--;
+
+    if (this.headIndex === -1) {
+      this.head = null;
+    } else if (this.elementsType === 'string') {
+      this.head = String.fromCharCode(this.array[this.headIndex]);
+    } else {
+      this.head = this.array[this.headIndex];
+    }
+
+    return deleted;
+  }
+}
+
+const stack = new Stack(Int32Array, 10);
+   
+stack.push(10);
+stack.push(11);
+stack.push(12);
+
+console.log(stack.head);  // 12
+
+console.log(stack.pop()); // 12
+
+console.log(stack.head);  // 11
+
+console.log(stack.pop()); // 11
+console.log(stack.pop()); // 10
+console.log(stack.pop()); // Exception
